0bd9fe669ab75ab0a6265cfdd9b3857c437f31ab
Tshenolo17 <ntshononohope@gmail.com>
2023-09-18 17:45:51 +0200

    added precious stone api

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const global_error_handler = require("./controllers/error_controller");
 const user_router = require("./routes/user_routes");
+const precious_stones_router = require("./routes/precious_stone_dealer_licence_routes")
 const app_error = require("./utils/app_error");
 
 //express application configs
@@ -12,6 +13,7 @@ app.use(cors());
 
 //endpoints
 app.use("/api/users", user_router);
+app.use("/api/precious-stones-dealer-licenses", precious_stones_router);
 app.use("*", (req, res, next) => {
     next(new app_error(`Can't find ${req.originalUrl} on this server!`, 404));
 });
---
+++ backend/controllers/applications_controller.js
@@ -0,0 +1,75 @@
+const catch_async = require("../utils/catch_async");
+const axios = require("axios");
+const {BASE_URL} = require("../utils/base_endpoints");
+
+
+exports.create_application = catch_async(async (req,res, next) => {
+    const config = {
+        method: "post",
+        url: `${BASE_URL}/api/collections/precious_stones_dealers_applications/records`,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        data: req.body,
+    }
+
+    await axios(config).then(function (response) {
+        res.status(200).json(response.data);
+    });
+});
+
+exports.update_application = catch_async(async (req, res, next) => {
+    const application_id = req.params.id;
+
+    const config = {
+        method: "patch",
+        url: `${BASE_URL}/api/collections/precious_stones_dealers_applications/records/${application_id}`,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        data: req.body,
+    };
+
+    await axios(config).then(function (response) {
+        res.status(200).json(response.data);
+    });
+});
+
+exports.get_applications = catch_async(async (req, res, next) => {
+    const {page, per_page} = req.query.page;
+
+    let config ={
+        method: "get",
+        url: `${BASE_URL}/api/collections/precious_stones_dealers_applications/records?page=${page}&per_page=${per_page}`
+    };
+
+
+    await axios(config).then(function (response) {
+        res.status(200).json(response.data);
+    });
+});
+
+exports.get_application_by_id = catch_async(async (req, res, next) => {
+    const application_id = req.params.id;
+    const config = {
+        method: "get",
+        url: `${BASE_URL}/api/collections/precious_stones_dealers_applications/records/${application_id}`,
+    };
+
+    await axios(config).then(function (response) {
+        res.status(200).json(response.data);
+    });
+});
+
+exports.delete_application = catch_async(async (req, res, next) => {
+    const application_id = req.params.id;
+
+    const config = {
+        method: "delete",
+        url: `${BASE_URL}/api/collections/precious_stones_dealers_applications/records/${application_id}`,
+    };
+
+    await axios(config).then(function (response) {
+        res.status(200).json(response.data);
+    });
+});
