72985de4e8ceacd0d43ad2d9ed4dc550603f4683
Godwill Monthe <50472662+godwillmonthe@users.noreply.github.com>
2023-09-18 16:27:09 +0200

    paypal integration

---
+++ frontend/src/views/components/paypalButton.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+function PaypalButton() {
+    const [paypalReady, setPaypalReady] = useState(false);
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = `https://www.paypal.com/sdk/js?client-id=AbnS0tQTmT1dkeisoVms2eZinp2mtkGvZwOPmeE2iS7KVo6NdZt6XFpsJM_qQ1VrdbWamqldmUIgCAPa`;
+        script.addEventListener('load', () => setPaypalReady(true));
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (paypalReady) {
+            window.paypal
+                .Buttons({
+                    createOrder: (data, actions) => {
+                        return actions.order.create({
+                            purchase_units: [
+                                {
+                                    amount: {
+                                        value: '100.00', // replace with the actual amount
+                                    },
+                                },
+                            ],
+                        });
+                    },
+                    onApprove: (data, actions) => {
+                        return actions.order.capture().then((details) => {
+                            alert('Transaction completed by ' + details.payer.name.given_name);
+                            // Call your server to save the transaction
+                        });
+                    },
+                })
+                .render('#paypal-button-container');
+        }
+    }, [paypalReady]);
+
+    if (!paypalReady) {
+        return <div>Loading...</div>;
+    }
+
+    return <div id="paypal-button-container"></div>;
+}
+
+export default PaypalButton;
